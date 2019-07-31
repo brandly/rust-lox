0d45b0f58e8fd7470ac8c595ac2dccbbb7194abf
brandly <brandly13@gmail.com>
2019-07-30 22:55:57 -0400

    match on args.len() :shrug:

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,21 @@ mod token;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() > 2 {
-        eprintln!("Usage: lox [script]");
-        process::exit(1);
-    } else if args.len() == 2 {
-        if let Err(e) = lox::run_file(&args[1]) {
-            eprintln!("Application error: {}", e);
-            process::exit(1);
+    match args.len() {
+        1 => {
+            if let Err(e) = lox::run_prompt() {
+                eprintln!("Application error: {}", e);
+                process::exit(1);
+            }
         }
-    } else {
-        if let Err(e) = lox::run_prompt() {
-            eprintln!("Application error: {}", e);
+        2 => {
+            if let Err(e) = lox::run_file(&args[1]) {
+                eprintln!("Application error: {}", e);
+                process::exit(1);
+            }
+        }
+        _ => {
+            eprintln!("Usage: lox [script]");
             process::exit(1);
         }
     }
