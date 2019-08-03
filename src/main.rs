b6f53cb51340c9447401e623e25d353e555b4475
brandly <brandly13@gmail.com>
2019-08-03 15:17:46 -0400

    match args as slice

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,15 @@ mod token;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    match args.len() {
-        1 => {
+    match args.as_slice() {
+        [_] => {
             if let Err(e) = lox::run_prompt() {
                 eprintln!("Application error: {}", e);
                 process::exit(1);
             }
         }
-        2 => {
-            if let Err(e) = lox::run_file(&args[1]) {
+        [_, filename] => {
+            if let Err(e) = lox::run_file(filename) {
                 eprintln!("Application error: {}", e);
                 process::exit(1);
             }
